aba28b2f23ec98fc14c1fc6f235de2f87eb9ec32
nurbek2107 <abduaxatov002@gmail.com>
2024-06-07 13:02:12 +0500

    main

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
 
   return (
     <div>
-      {isPending && <div className="rounded-lg p-4 max-w-10 mx-auto h-lvh ">    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" width="200" height="200" style={{ shapeRendering: 'auto', display: 'block' }}>
+      {isPending && <div className="rounded-lg pt-4 max-w-8 mx-auto h-lvh ">    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" width="200" height="200" style={{ shapeRendering: 'auto', display: 'block' }}>
         <g>
           <circle strokeLinecap="round" fill="none" strokeDasharray="50.26548245743669 50.26548245743669" stroke="#e15b64" strokeWidth="8" r="32" cy="50" cx="50">
             <animateTransform values="0 50 50;360 50 50" keyTimes="0;1" repeatCount="indefinite" dur="1.1904761904761905s" type="rotate" attributeName="transform"></animateTransform>
